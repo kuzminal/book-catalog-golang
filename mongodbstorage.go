0807149fae3d82517a821ff0862e64ba8379e322
kuzminal <kuzmin.al35@gmail.com>
2021-11-16 11:55:47 +0300

    Пока два метода работают - GET и POST

---
+++ mongodbstorage.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"time"
+)
+
+var collection *mongo.Collection
+var ctx = context.TODO()
+
+func init() {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017/")
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	collection = client.Database("book-catalog").Collection("books")
+}
+
+func SaveBook(book Book) error {
+	//Perform InsertOne operation & validate against the error.
+	_, err := collection.InsertOne(context.TODO(), book)
+	if err != nil {
+		return err
+	}
+	//Return success without any error.
+	return nil
+}
+
+func GetAll() ([]*Book, error) {
+	var tasks []*Book
+	filter := bson.D{primitive.E{Key: "", Value: nil}}
+	cur, err := collection.Find(ctx, filter)
+	if err != nil {
+		return tasks, err
+	}
+	// Iterate through the cursor and decode each document one at a time
+	for cur.Next(ctx) {
+		var t Book
+		err := cur.Decode(&t)
+		if err != nil {
+			return tasks, err
+		}
+		tasks = append(tasks, &t)
+	}
+	if err := cur.Err(); err != nil {
+		return tasks, err
+	}
+	// once exhausted, close the cursor
+	cur.Close(ctx)
+	if len(tasks) == 0 {
+		return tasks, mongo.ErrNoDocuments
+	}
+	return tasks, nil
+}
+
+func GetBook(isbn string) (Book, error) {
+	result := Book{}
+	filter := bson.D{primitive.E{Key: "isbn", Value: isbn}}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	err := collection.FindOne(ctx, filter).Decode(&result)
+	if err == mongo.ErrNoDocuments {
+		// Do something when no record was found
+		fmt.Println("record does not exist")
+	} else if err != nil {
+		log.Fatal(err)
+		return result, err
+	}
+	return result, nil
+}
